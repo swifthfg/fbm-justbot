71b588ece5bb99f1f94ccf25ddf6395211655413
Faruk <gehafar@gmail.com>
2018-08-10 21:43:28 +0300

    Change app logic

--- index.js
+++ index.js
@@ -1,162 +1,62 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
-var app = express()
-require('dotenv').config()
 
-app.set('port', (process.env.PORT || 5000))
 
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+const
+	request = require('request'),
+	express = require('express'),
+	body_parser = require('body-parser'),
+	app = express().use(body_parser.json());
+	require('dotenv').config()
 
-// Process application/json
-app.use(bodyParser.json())
+app.listen(process.env.PORT || 1337, () => console.log('Webhook is listening'));
 
-// Index route
-app.get('/', function (req, res) {
-	res.send('Hello world, I am a chat bot')
-})
+app.post('/webhook', (req, res) => {
 
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'justbotter') {
-		res.send(req.query['hub.challenge'])
+	// Parse the request body from the POST
+	let body = req.body;
+
+	// Check the webhook event is from a Page subscription
+	if (body.object === 'page') {
+
+		// Iterate over each entry - there may be multiple if batched
+		body.entry.forEach(function(entry) {
+
+			// Get the webhook event. entry.messaging is an array, but
+			// will only ever contain one event, so we get index 0
+			let webhook_event = entry.messaging[0];
+			console.log(webhook_event);
+	});
+
+	// Return a '200 OK' response to all events
+	res.status(200).send('EVENT_RECEIVED');
+
+	} else {
+		// Return a '404 Not Found' if event is not from a page subscription
+		res.sendStatus(404);
 	}
-	res.send('Error, wrong token')
-})
 
-// Spin up the server
-app.listen(app.get('port'), function() {
-	console.log('running on port', app.get('port'))
-})
+});
 
+// Accepts GET requests at the /webhook endpoint
+app.get('/webhook', (req, res) => {
 
-// API End Point - added by Stefan
+	const VERIFY_TOKEN = process.env.PAGE_TOKEN;
 
-app.post('/webhook/', function (req, res) {
-	messaging_events = req.body.entry[0].messaging
-	for (i = 0; i < messaging_events.length; i++) {
-		event = req.body.entry[0].messaging[i]
-		sender = event.sender.id
-		if (event.message && event.message.text) {
-			text = event.message.text
-			if (text === 'hi') {
-				sendGenericMessage(sender)
-				continue
-			}
-			sendTextMessage(sender, "parrot: " + text.substring(0, 200))
-		}
-		if (event.postback) {
-			text = JSON.stringify(event.postback)
-			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
-			continue
-		}
-	}
-	res.sendStatus(200)
-})
+	let mode = req.query['hub.mode'];
+	let token = req.query['hub.verify_token'];
+	let challenge = req.query['hub.challenge'];
 
-var token = process.env.PAGE_TOKEN
+	// Check if a token and mode were sent
+	if (mode && token) {
+		// Check the mode and token sent are correct
+		if (mode === 'subscribe' && token === VERIFY_TOKEN) {
 
-// function to echo back messages
+			// Respond with 200 OK and challenge token from the request
+			console.log('WEBHOOK_VERIFIED');
+			res.status(200).send(challenge);
 
-function sendTextMessage(sender, text) {
-	messageData = {
-		text:text
-	}
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
-}
-
-
-// Send a test message back as two cards.
-
-function sendGenericMessage(sender) {
-	messageData = {
-		"attachment": {
-			"type": "template",
-			"payload": {
-				"template_type": "generic",
-				"elements": [{
-					"title": "Ai Chat Bot Communities",
-					"subtitle": "Communities to Follow",
-					"image_url": "http://1u88jj3r4db2x4txp44yqfj1.wpengine.netdna-cdn.com/wp-content/uploads/2016/04/chatbot-930x659.jpg",
-					"buttons": [{
-						"type": "web_url",
-						"url": "https://www.facebook.com/groups/aichatbots/",
-						"title": "FB Chatbot Group"
-					}, {
-						"type": "web_url",
-						"url": "https://www.reddit.com/r/Chat_Bots/",
-						"title": "Chatbots on Reddit"
-					},{
-						"type": "web_url",
-						"url": "https://twitter.com/aichatbots",
-						"title": "Chatbots on Twitter"
-					}],
-				}, {
-					"title": "Chatbots FAQ",
-					"subtitle": "Aking the Deep Questions",
-					"image_url": "https://tctechcrunch2011.files.wordpress.com/2016/04/facebook-chatbots.png?w=738",
-					"buttons": [{
-						"type": "postback",
-						"title": "What's the benefit?",
-						"payload": "Chatbots make content interactive instead of static",
-					},{
-						"type": "postback",
-						"title": "What can Chatbots do",
-						"payload": "One day Chatbots will control the Internet of Things! You will be able to control your homes temperature with a text",
-					}, {
-						"type": "postback",
-						"title": "The Future",
-						"payload": "Chatbots are fun! One day your BFF might be a Chatbot",
-					}],
-				},  {
-					"title": "Learning More",
-					"subtitle": "Aking the Deep Questions",
-					"image_url": "http://www.brandknewmag.com/wp-content/uploads/2015/12/cortana.jpg",
-					"buttons": [{
-						"type": "postback",
-						"title": "AIML",
-						"payload": "Checkout Artificial Intelligence Mark Up Language. Its easier than you think!",
-					},{
-						"type": "postback",
-						"title": "Machine Learning",
-						"payload": "Use python to teach your maching in 16D space in 15min",
-					}, {
-						"type": "postback",
-						"title": "Communities",
-						"payload": "Online communities & Meetups are the best way to stay ahead of the curve!",
-					}],
-				}]
-			}
+		} else {
+			// Responds with '403 Forbidden' if verify tokens do not match
+			res.sendStatus(403);
 		}
 	}
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
-}
+});
