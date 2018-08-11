3555be3d4a6010d1213a9d335ed9b43f391cc280
Faruk <gehafar@gmail.com>
2018-08-11 10:58:00 +0300

    Add functionality to send message

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const
 	request = require('request'),
 	express = require('express'),
@@ -7,16 +5,26 @@ const
 	app = express().use(body_parser.json());
 	require('dotenv').config()
 
-app.listen(process.env.PORT || 1337, () => console.log('Webhook is listening'));
+app.listen(process.env.PORT || 1337, () => console.log('Webhook is up'));
 
 app.post('/webhook', (req, res) => {
-
-	// Parse the request body from the POST
 	let body = req.body;
 
 	// Check the webhook event is from a Page subscription
 	if (body.object === 'page') {
+		let messagingEvents = req.body.entry[0].messaging
+		for (let i = 0; i < messagingEvents.length; i++) {
+			let mEvent = messagingEvents[0]
+			let sender = mEvent.sender.id
+			if (mEvent.message && mEvent.message.text) {
+				let text = mEvent.message.text
+				sendText(sender, "Echo: " + text)
+			}
+		}
+		res.sendStatus(200)
 
+
+		/*
 		// Iterate over each entry - there may be multiple if batched
 		body.entry.forEach(function(entry) {
 
@@ -24,38 +32,52 @@ app.post('/webhook', (req, res) => {
 			// will only ever contain one event, so we get index 0
 			let webhook_event = entry.messaging[0];
 			console.log(webhook_event);
-	});
+		});
 
-	// Return a '200 OK' response to all events
-	res.status(200).send('EVENT_RECEIVED');
+		// Return a '200 OK' response to all events
+		res.status(200).send('EVENT_RECEIVED');
+		*/
+
 
 	} else {
-		// Return a '404 Not Found' if event is not from a page subscription
 		res.sendStatus(404);
 	}
 
 });
 
-// Accepts GET requests at the /webhook endpoint
-app.get('/webhook', (req, res) => {
+function sendText(sender, textMessage) {
+	let messageData = {text: textMessage}
+	let receiptData = {id: sender}
+	request({
+		url: "https://graph.facebook.com/v2.6/me/messages",
+		qs: {access_token: process.env.TOKEN},
+		method: "POST",
+		json: {
+			receipt: receiptData,
+			message: messageData
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log("error occured");
+		} else if (response.body.error) {
+			console.log("response body error occured");
+		}
 
+	})
+}
+
+app.get('/webhook', (req, res) => {
 	const VERIFY_TOKEN = process.env.TOKEN;
 
 	let mode = req.query['hub.mode'];
 	let token = req.query['hub.verify_token'];
 	let challenge = req.query['hub.challenge'];
 
-	// Check if a token and mode were sent
 	if (mode && token) {
-		// Check the mode and token sent are correct
 		if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-			// Respond with 200 OK and challenge token from the request
 			console.log('WEBHOOK_VERIFIED');
 			res.status(200).send(challenge);
-
 		} else {
-			// Responds with '403 Forbidden' if verify tokens do not match
 			res.sendStatus(403);
 		}
 	}
